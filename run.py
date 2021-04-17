c973bcf1af26591aa0d554d747d53e757b2b0447
rohanmandrekar <rmaskaban@gmail.com>
2021-04-17 14:18:02 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import timeit
 import random
-import sys
 
 length=int(input('enter length of input numbers : '))
 
@@ -114,111 +113,159 @@ def make_binarysearchtree(inputs,querry):
 	print('runtime for binary search tree : ',stop-start)	
 
 
-class Null:
-	def __init__(self,parent):
-		self.data=None
-		self.left=None
-		self.right=None
-		self.color='black'
-		self.parent=parent
+class NullNode:
+	def __init__(self, parent):
+		self.val = None
+		self.left = None
+		self.right = None
+		self.parent = parent
+		self.color = "BLACK"
 
 class RBNode:
-	def __init__(self,data):
-		self.data=data
-		self.left=Null(self)
-		self.right=Null(self)
-		self.parent=None
-		self.color='red'
-
-
-# reference : https://github.com/JinheonBaek/DS-RedBlackTree/blob/master/term-project/tree.py
-
-class redblacktree:
-
-	def __init__(self):
-		self.root=None
-
-	def insert_node(self,tree,n):
-
-		root=self.root
-		y=None
-
-		while root is not None and root.data is not None:
-
-			y=root
-
-			if (n.data<root.data):
-				root= root.left
-
-			else:
-				root= root.right	
-
-			n.parent=y
-			if y is None:
-				self.root = n
-			elif n.data < y.data:
-			    y.left = n
-			else:
-			    y.right = n
-
-		self.fixRBT(sef,n)
-
-	def find(self, tree , querry):
-
-		
-		if tree.data > querry:
-			return self.search(tree.left, querry)
-		elif tree.data < querry:
-			return self.search(tree.right, querry)
-		else:
-			print('querry found in red black tree')
-			return tree		    				
-		if tree.data is None:
-			print(querry, "is not in the red black tree")
-			return tree
-
-	def fixRBT(self, tree, n):
-		while n.parent is not None and n.parent.parent is not None and n.parent.color is 'red':
-			if n.parent = n.parent.parent.left:
-				y=n.parent.parent.right:
-				if y is not None and y.color=='red':
-					n.parent.color='black'
-					y.color='black'
-					n.parent.parent.color='red'
-					n=n.parent.parent
-				else:
-					if n== n.parent.right:
-						n=n.parent
-						self.left_rotate(tree,n)
-					n.parent.color='red'
-					n.parent.parent.color='black'
-					self.right_rotate(tree,n.parent.parent)
-
-			else:
-				y=n.parent.parent.left
-				if y is not None and y.color=='red'
-					n.parent.color='black'
-					y.color='black'
-					n.parent.parent.color='red'
-					n=n.parent.parent
-				else:
-					if n==n.parent.left
-						n=n.parent
-						self.right_rotate(tree,n)
-
-					n.parent.color='black'
-					n.parent.parent.color='red'
-					self.left_rotate(tree, n.parent.parent)
-
-			self.root.color='black'
-
-	def left_rotate(self, tree, x):
-	
-	def right_rotate(self, tree, x):
-
+	def __init__(self, newval):
+		self.val = newval
+		self.left = NullNode(self)
+		self.right = NullNode(self)
+		self.parent = None
+		self.color = 'RED'
 
 
         
+# https://github.com/JinheonBaek/DS-RedBlackTree/blob/master/term-project/tree.py
+class redblacktree:
+	def __init__(self):
+		self.root = None
+
+	def search(self, tree, val):
+		if self.root is None:
+			#print("That RedBlack Tree do not have any Node")
+			return None
+		if tree.val is None:
+			print(val, "is not in the red-black tree")
+			return tree
+		if tree.val > val:
+			return self.search(tree.left, val)
+		elif tree.val < val:
+			return self.search(tree.right, val)
+		else:
+			print('querry found in red-black tree')
+			return tree
+
+	def insert(self, tree, n):
+			# init
+			y = None
+			x = self.root
+			# self.insertNode += 1
+
+			# search inserting place
+			while x is not None and x.val is not None:
+				y = x
+				if n.val < x.val:
+				    x = x.left
+				else:
+				    x = x.right
+
+				# insert new node
+			n.parent = y
+			if y is None:
+				self.root = n
+			elif n.val < y.val:
+				y.left = n
+			else:
+				y.right = n
+
+			# fix up RBT
+			self.RBT_Insert_Fixup(self, n)
+
+	def RBT_Insert_Fixup(self, tree, n):
+		while n.parent is not None and n.parent.parent is not None and n.parent.color is 'RED':
+			if n.parent == n.parent.parent.left:
+				y = n.parent.parent.right
+				if y is not None and y.color == "RED":
+					n.parent.color = "BLACK"
+					y.color = "BLACK"
+					n.parent.parent.color = "RED"
+					n = n.parent.parent
+				else:
+					if n == n.parent.right:
+						n = n.parent
+						self.Left_Rotate(tree, n)
+					n.parent.color = "BLACK"
+					n.parent.parent.color = "RED"
+					self.Right_Rotate(tree, n.parent.parent)
+			else:
+				y = n.parent.parent.left
+				if y is not None and y.color == "RED":
+					n.parent.color = "BLACK"
+					y.color = "BLACK"
+					n.parent.parent.color = "RED"
+					n = n.parent.parent
+				else:
+					if n == n.parent.left:
+						n = n.parent
+						self.Right_Rotate(tree, n)
+					n.parent.color = "BLACK"
+					n.parent.parent.color = "RED"
+					self.Left_Rotate(tree, n.parent.parent)
+		self.root.color = "BLACK"
+
+	def Left_Rotate(self, tree, x):
+            # set y
+		y = x.right
+
+		# insert subtree of y into x
+		x.right = y.left
+		if y.left is not None:
+			y.left.parent = x
+
+		# link x's parent to y
+		y.parent = x.parent
+		if x.parent is None:
+			tree.root = y
+		elif x == x.parent.left:
+			x.parent.left = y
+		else:
+			x.parent.right = y
+
+		# put x on y's left
+		y.left = x
+		x.parent = y
+
+	def Right_Rotate(self, tree, x):
+		# set y
+		y = x.left
+
+		# insert subtree of y into x
+		x.left = y.right
+		if y.right is not None:
+			y.right.parent = x
+
+		# link x's parent to y
+		y.parent = x.parent
+		if x.parent is None:
+			tree.root = y
+		elif x == x.parent.left:
+			x.parent.left = y
+		else:
+			x.parent.right = y
+
+		# put x on y's right
+		y.right = x
+		x.parent = y
+
+
+def make_redblacktree(inputs,querry):
+
+	rbt=redblacktree()
+
+	for i in inputs:
+		rbt.insert(rbt.root,RBNode(i))
+	start=timeit.default_timer()	
+	rbt.search(rbt.root,querry)	
+	stop=timeit.default_timer()
+	print('search time for red black tree : ',stop-start)
+
+
 
 	
 linearsearch(inputs,querry)	
@@ -227,5 +274,5 @@ binarysearch(inputs,querry)
 
 make_binarysearchtree(inputs,querry)
 
-# make_redblacktree(inputs,20)
+make_redblacktree(inputs,querry)
 
